12c4b54218255c6d6d8d0061cca27d2d43271884
Dario Rodriguez <dariodis97@gmail.com>
2026-05-22 11:24:06 -0300

    cambio wsgi

--- wsgi.py
+++ wsgi.py
@@ -1,10 +1,8 @@
 import sys
 import os
-
-# Añadir la carpeta app al path
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), 'app')))
+sys.path.insert(0, os.path.abspath('app'))
 
 from app import app
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
